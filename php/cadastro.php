5376b4acda80ec9591fc1d3a787fe530f0a5c7d0
Vahrnae <145216228+Vahrnae@users.noreply.github.com>
2024-04-24 15:21:02 -0300

    v0.13
    Conserto do cadastro, ausência da finalização dos outros campos de texto

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,17 +1,16 @@
 <?php
-$nome = $_POST['input-nome-completo'];
-$username = $_POST['input-username'];
-$Senha = $_POST['input-senha'];
-$checksenha = $_POST['input-repetir-senha'];
-$email = $_POST['input-email'];
-$cpf = $_POST['input-cpf'];
-$telefone = $_POST['input-tel'];
-$endereco = $_POST['input-endereco'];
-$cep = $_POST['input-cep'];
+$nome = $_POST['nome-completo'];
+$username = $_POST['username'];
+$Senha = $_POST['senha'];
+$email = $_POST['email'];
+$cpf = $_POST['cpf'];
+$telefone = $_POST['tel'];
+$endereco = $_POST['endereco'];
+$cep = $_POST['cep'];
 
 include('connection.php');
 
-    $sql = "INSERT into clientes(nome, username, email, senha, telefone, cpf, endereco, cep) VALUES('$nome','$username','$email','$Senha','$telefone','$cpf','$endereco','$cep')";
+    $sql = "INSERT into clientes(nome, username, email, senha, telefone, cpf, endereco, cep) VALUES('$nome', '$username', '$email', '$Senha', '$telefone', '$cpf', '$endereco', '$cep')";
 
     $rs = mysqli_query($mysqli, $sql);
 
