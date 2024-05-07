57ee412d9542b02eac7b2e3c8595d9b2dabd72ae
VesteVerso <145216228+Vahrnae@users.noreply.github.com>
2024-05-07 15:44:12 -0300

    v0.17
    Início do login, ainda é necessário que faça alterações para o funcionamento do login

---
+++ php/login.php
@@ -0,0 +1,47 @@
+<?php
+include("connection.php");
+
+if(isset($_POST['User']) || isset($_POST['Password'])) 
+{
+    if(strlen($_POST['User']) == 0)
+    {
+        echo("Preencha seu usuário");
+    }
+    elseif(strlen($_POST['Password']) == 0)
+    {
+        echo("Preencha sua senha");
+    }
+    else
+    {
+        $usuario = $mysqli->real_escape_string($_POST['User']);
+        $Senha = $mysqli->real_escape_string($_POST['Password']);
+
+
+        $sql_code = "SELECT * FROM clientes WHERE username = '$usuario' AND senha = '$Senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error) ;
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1)
+        {
+
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION))
+            {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: ../html/index.html");
+
+        }
+        else
+        {
+            echo("Falha ao logar! E-Mail ou Senha incorretos");
+        }
+    }
+}
+?>
