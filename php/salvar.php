c6c0c5be93e72bec1ac341ae7a7417974b90e2ce
VesteVerso <163040963+VesteVerso@users.noreply.github.com>
2024-05-23 16:04:07 -0300

    v0.21
    Imagem.

--- php/salvar.php
+++ php/salvar.php
@@ -4,10 +4,11 @@ include('connection.php');
 $nome = $mysqli->real_escape_string($_POST['nome-produto']);
 $preco = $mysqli->real_escape_string($_POST['preco']);
 $categoria = $mysqli->real_escape_string($_POST['gridRadios']);
+$img = $mysqli->real_escape_string($_POST['imagem']);
 
-$sql = "INSERT into produto(categoria, preco, nome_prod) VALUES('$categoria','$preco','$nome')";
+$sql = "INSERT into produto(categoria, preco, nome_prod, img) VALUES('$categoria','$preco','$nome','$img')";
 
     $rs = mysqli_query($mysqli, $sql);
 
-    header("Location: ../html/cadastro-produto.html");
+    header("Location:../html/cadastro-produto.html");
 ?>
