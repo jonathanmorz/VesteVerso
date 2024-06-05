990e2acab4c5c78bd5e2cea4819af304f14d04b3
VesteVerso <163040963+VesteVerso@users.noreply.github.com>
2024-06-05 15:04:52 -0300

    v0.22.2
    Finalização do cadastro de produto

--- php/salvar.php
+++ php/salvar.php
@@ -4,11 +4,39 @@ include('connection.php');
 $nome = $mysqli->real_escape_string($_POST['nome-produto']);
 $preco = $mysqli->real_escape_string($_POST['preco']);
 $categoria = $mysqli->real_escape_string($_POST['gridRadios']);
-$img = $mysqli->real_escape_string($_POST['imagem']);
 
-$sql = "INSERT into produto(categoria, preco, nome_prod, img) VALUES('$categoria','$preco','$nome','$img')";
+if(isset($_FILES['arquivo'])) {
+    $arquivo = $_FILES['arquivo'];
+
+        if($arquivo['error'])
+            die("Falha ao enviar arquivo");
+
+        if($arquivo['size'] > 4194304)
+            die("Arquivo muito grande! Max: 4MB");
+
+    $pasta = "arquivos/";
+    $nomeDoArquivo = $arquivo['name'];
+    $novoNomeDoArquivo = uniqid();
+    $extensao = strtolower(pathinfo($nomeDoArquivo,PATHINFO_EXTENSION));
+
+    if($extensao != "jpg" && $extensao != "png")
+        die("Tipo de arquivo não aceito, necessário ser jpg ou png.");
+
+    $path = $pasta . $novoNomeDoArquivo . "." . $extensao;    
+
+    $deu_certo = move_uploaded_file($arquivo['tmp_name'], $pasta . $novoNomeDoArquivo . "." . $extensao);
+
+    if($deu_certo)
+    {
+        $sql = "INSERT into produto(categoria, preco, nome_prod, path) VALUES('$categoria','$preco','$nome','$path')";
 
     $rs = mysqli_query($mysqli, $sql);
 
     header("Location:../html/cadastro-produto.html");
-?>
+
+    }
+
+    else
+        echo "Falha ao enviar";
+
+}
