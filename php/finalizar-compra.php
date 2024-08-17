cb3cbb2ce93ee5584e1fc71ada01acb83a04a66d
Yuri Lopes <136222993+itsyuzzy@users.noreply.github.com>
2024-08-17 15:21:14 -0300

    v0.41.1
    - Ajudei o Daniel a centralizar a div
    - Adicionei um teste de ajax em javascript

--- php/finalizar-compra.php
+++ php/finalizar-compra.php
@@ -21,47 +21,45 @@
     echo htmlHeaderNoNavBar($username, $role);
   ?>
 <div id="div-geral">
-            <form id="form-pagamento">
-                <div id="conteudo-formulario">
-                    <div id="finalizar-compra">
-                        <h1>Finalizar compra</h1>
-                    </div>
-                        <div id="linha-preta-div"><img src="../resources/images/pixel-linha.jpg" alt="linha preta" id="linha-preta"></div>
-                    <div id="selecionar-titulo">
-                        <h2>Selecione sua forma de pagamento:</h2>
-                    </div>
-                    <div id="pix" class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="radio-pix" value="opcao1" checked>
-                        <label class="form-check-label" for="radio-pix">
-                            PIX
-                            </div>
-                        </label>
-                       
-                        <img src="../resources/images/logo-pix.png" width="50" height="50">
-                        </div>
-                        <div id="credito-debito" class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="radio-credito" value="opcao2">
-                        <label class="form-check-label" for="radio-credito">
-                            Crédito ou Débito
-                        </label>
-                        <img src="../resources/images/logo-cartao.png" width="50" height="50">
-                        </div>
-                        <div id="boleto" class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="radio-boleto" value="opcao4">
-                        <label class="form-check-label" for="radio-boleto">
-                            Boleto
-                        </label>
-                        <img src="../resources/images/logo-boleto.png" width="50" height="50">
-                        </div>
+    <form id="form-pagamento">
+        <div id="conteudo-formulario">
+            <div id="finalizar-compra">
+                <h1>Finalizar compra</h1>
+            </div>
 
-                    </div>  
+            <div id="linha-preta-div">
+                <img src="../resources/images/pixel-linha.jpg" alt="linha preta" id="linha-preta">
+            </div>
 
-                        <div id="div-botao-pagamento"><button id="botao-pagamento">Continuar</button></div>
+            <div id="selecionar-titulo">
+                <h2>Selecione sua forma de pagamento:</h2>
+            </div>
+
+            <div id="metodos-pagamento">
+                <div id="pix" class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="radio-pix" value="opcao1" checked>
+                    <label class="form-check-label" for="radio-pix">PIX</label>
+                    <img src="../resources/images/logo-pix.png" width="50" height="50">
                 </div>
-            </form>
-    </div>
+                <div id="credito-debito" class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="radio-credito" value="opcao2">
+                    <label class="form-check-label" for="radio-credito">Crédito ou Débito</label>
+                    <img src="../resources/images/logo-cartao.png" width="50" height="50">
+                </div>
+                <div id="boleto" class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="radio-boleto" value="opcao4">
+                    <label class="form-check-label" for="radio-boleto">Boleto</label>
+                    <img src="../resources/images/logo-boleto.png" width="50" height="50">
+                
+                </div>
+            </div>
 
-  </div>
+            <div id="div-botao-pagamento"><button id="botao-pagamento">Continuar</button></div>
+        </div>
+    </form>
+
+
+</div>
     
   <?php
    echo htmlFooter();
