7cc2d0753239c74bc178415f68b2b9f98a39ad8d
VesteVerso <vesteverso287@gmail.com>
2024-09-17 17:19:49 -0300

    v0.48.7
    -Criado página para usuários que esqueceram senha e desejam recupera-la
    -Estilizações

---
+++ php/esquecer-senha.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esqueceu a Senha?</title>
+    <link rel="stylesheet" href="../css/esquecer-senha.css">
+</head>
+<body>
+    <div class="global">
+        <div class="esquerda">
+            <div class="icone-info">
+                <img src="../resources/images/logo2-roxo.png" alt="Ícone">
+            </div>
+            <h2>Informação importante!</h2>
+            <p>Por favor leia as informações abaixo, e compartilhe os dados necessários.</p>
+            <ul>
+                <li>Não revele sua senha para ninguém.</li>
+                <li>Não use senhas repetidas.</li>
+                <li>Use senhas alfanuméricas.</li>
+            </ul>
+        </div>
+
+        <div class="direita">
+            <h2>Esqueceu a senha?</h2>
+            <p> Não se preocupe, nós podemos ajudar.</p>
+            <form>
+                <input type="email" id="email" placeholder="Insira seu endereço de e-mail">
+                <p class="instrucoes">
+                    Se existir uma conta com o e-mail informado, você receberá um e-mail com as instruções para recuperar sua conta.
+                </p>
+                <button type="submit">Continuar</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
