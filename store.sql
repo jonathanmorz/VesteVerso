63831cb659be607a1daae14eb0cd73fc531b0e1d
Yuri Lopes <136222993+itsyuzzy@users.noreply.github.com>
2024-09-29 01:11:54 -0300

    v0.50.5
    - Banco de dados atualizado

--- store.sql
+++ store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03/08/2024 às 07:56
+-- Tempo de geração: 29/09/2024 às 06:10
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -41,10 +41,13 @@ CREATE TABLE `carrinho` (
 --
 
 INSERT INTO `carrinho` (`id`, `user_id`, `produto_id`, `quantidade`, `criado_em`, `atualizado_em`) VALUES
-(10, 4, 1, 1, '2024-07-18 00:22:05', '2024-07-18 00:22:05'),
-(11, 4, 2, 3, '2024-07-18 00:22:05', '2024-07-18 00:22:05'),
-(18, 1, 3, 1, '2024-07-28 07:53:42', '2024-07-28 07:53:42'),
-(19, 1, 4, 1, '2024-07-28 07:53:42', '2024-07-28 07:53:42');
+(28, 4, 1, 1, '2024-08-11 01:08:10', '2024-08-11 01:08:10'),
+(29, 4, 2, 3, '2024-08-11 01:08:10', '2024-08-11 01:08:10'),
+(32, 1, 3, 1, '2024-09-03 00:33:56', '2024-09-03 00:33:56'),
+(33, 1, 4, 1, '2024-09-03 00:33:56', '2024-09-03 00:33:56'),
+(34, 26, 3, 1, '2024-09-21 00:21:12', '2024-09-21 00:21:12'),
+(35, 26, 4, 1, '2024-09-21 00:21:12', '2024-09-21 00:21:12'),
+(36, 27, 3, 1, '2024-09-21 00:27:09', '2024-09-21 00:27:09');
 
 -- --------------------------------------------------------
 
@@ -73,7 +76,16 @@ INSERT INTO `clientes` (`id`, `nome`, `username`, `email`, `senha`, `telefone`, 
 (1, 'Yuri Lopes', 'yuzzy', 'tpnvlnd@gmail.com', 'luiz1227', '21982893856', '140.961.387', 'Rua Frei Alexandre', '21230070', 'admin'),
 (2, 'dwads', '', '', '', '', '', '', '', 'cliente'),
 (3, 'Tico Teco', 'Tetoticoteco', 'Tecotico@gmail.com', '12345678', '21982893856', '29712474647', 'Rua Frei Alexandre', '21230070', 'cliente'),
-(4, 'Daniel Dias', 'danielnoites07', 'robloxlandia456123@gmail.com', 'Ba456123', '21 982324256', '02374802753', 'Rua Gonçalves Crespo, 22', '25032904', 'cliente');
+(4, 'Daniel Dias', 'danielnoites07', 'robloxlandia456123@gmail.com', 'Ba456123', '21 982324256', '02374802753', 'Rua Gonçalves Crespo, 22', '25032904', 'cliente'),
+(21, 'Admin', 'Admin', 'admin@admin.com', '', '', '12345667890', '', '', 'cliente'),
+(22, 'Admin', 'Admin', 'admin@admin.com', '', '', '12345667890', '', '', 'cliente'),
+(23, 'Admin', 'Admin', 'admin@admin.com', '', '', '12345667890', '', '', 'cliente'),
+(24, 'Admin', 'Admin', 'admin@admin.com', '', '', '12345667890', '', '', 'cliente'),
+(25, 'wkajkldj', 'aaabbb', 'aaabbb@gmail.com', 'aaabbb', '321331', '1232131', '3131', '31313', 'cliente'),
+(26, 'Teste', 'teste', 'teste@gmail.com', 'teste123', '11111111111', '11111111111', 'Teste', '11111111', 'cliente'),
+(27, 'Teste', 'teste', 'teste@gmail.com', 'teste', 'teste', 'teste', 'teste', 'teste', 'cliente'),
+(28, 'Teste', 'teste', 'teste@gmail.com', 'teste', 'teste', 'teste', 'teste', 'teste', 'cliente'),
+(29, 'teste', 'teste', 'teste@gmail.com', 'teste', 'teste', 'teste', 'teste', 'teste', 'cliente');
 
 -- --------------------------------------------------------
 
@@ -85,7 +97,6 @@ CREATE TABLE `favoritos` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `produto_id` int(11) NOT NULL,
-  `quantidade` int(11) NOT NULL,
   `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
   `atualizado_em` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -113,9 +124,9 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `quantidade`, `preco`, `imagem`, `categoria`, `descricao`, `em_alta`, `promocao`) VALUES
-(1, 'Camisa de Gola', 100, 56.90, 'arquivos/668dbacead06e.jpg', 'roupa_fem', 'Camisa de Gola Feminina Branca', 1, 0),
-(2, 'Camisa Longa', 100, 42.30, 'arquivos/0002.jpeg', 'roupa_fem', 'Camisa Manga Longa Feminina ', 1, 0),
-(3, 'Camisa Lilás', 100, 72.50, 'arquivos/668dbb42822eb.jpeg', 'roupa_fem', 'Camisa Lilás Feminina', 1, 0),
+(1, 'Teste', 100, 56.90, 'arquivos/668dbacead06e.jpg', 'roupa_fem', 'Camisa de Gola Feminina Branca', 1, 0),
+(2, 'Teste', 100, 42.30, 'arquivos/0002.jpeg', 'roupa_fem', 'Teste', 1, 0),
+(3, 'Teste', 100, 72.50, 'arquivos/668dbb42822eb.jpeg', 'roupa_fem', 'Teste', 1, 0),
 (4, 'Camisa Curta', 100, 35.60, 'arquivos/668dbb6d08172.jpeg', 'roupa_fem', 'Camisa Manga Curta Feminina', 1, 0),
 (5, 'Camiseta Oversized ', 100, 92.20, 'arquivos/668dbce26760d.jpeg', 'roupa_fem', 'Camiseta Oversized Feminina Branca', 1, 0),
 (6, 'Cropped Rosa', 100, 125.40, 'arquivos/668dbd39286e0.jpeg', 'roupa_fem', 'Cropped Rosa Feminino Com Manga', 0, 0),
@@ -128,7 +139,8 @@ INSERT INTO `produtos` (`id`, `nome`, `quantidade`, `preco`, `imagem`, `categori
 (13, 'Camisa Social', 100, 98.20, 'arquivos/668dbe28d4ad6.jpeg', 'roupa_masc', 'Camisa Social Com Botão', 1, 0),
 (14, 'Camisa Gola Alta', 100, 125.40, 'arquivos/668dbe868622b.jpeg', 'roupa_masc', 'Camisa Gola Alta Masculina', 1, 0),
 (15, 'Camiseta Verde', 100, 84.70, 'arquivos/668f0f404583c.jpeg', 'roupa_masc', 'Camiseta Gráfica Verde', 1, 0),
-(16, 'Camiseta Curta', 100, 63.80, 'arquivos/66a6118d18d05.jpeg', 'roupa_masc', 'Camiseta de Manga Curta', 0, 0);
+(16, 'Camiseta Curta', 100, 63.80, 'arquivos/66a6118d18d05.jpeg', 'roupa_masc', 'Camiseta de Manga Curta', 0, 0),
+(17, 'Teste', 100, 99.99, 'arquivos/66ee15b9c3d3c.jpeg', 'roupa_inf', 'Produto Teste', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -164,7 +176,8 @@ ALTER TABLE `clientes`
 -- Índices de tabela `favoritos`
 --
 ALTER TABLE `favoritos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_produto_unique` (`user_id`,`produto_id`);
 
 --
 -- Índices de tabela `produtos`
@@ -187,13 +200,13 @@ ALTER TABLE `user_sessions`
 -- AUTO_INCREMENT de tabela `carrinho`
 --
 ALTER TABLE `carrinho`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de tabela `favoritos`
@@ -205,7 +218,7 @@ ALTER TABLE `favoritos`
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `user_sessions`
